13dc0bd06515d8d08d96c6d8e42b1c915cc9e7cc
pandit <dhamdherepandit@gmail.com>
2022-12-27 18:23:31 +0530

    flip added

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { FaBars, FaExternalLinkAlt, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 const Navbar = () => {
 
     const [nav, setNav] = useState(false)
@@ -38,9 +38,24 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
-            <div onClick={() => setNav(!nav) } className='cursor-pointer pr-4 z-10 text-blue-500'>
-                {nav ? <FaTimes size={30} /> : <FaBars size={30} /> }
+            <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-blue-500'>
+                {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
+
+            {nav && (
+    <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-blue-800 text-blue-500'>
+    {links.map(({ id, link }) => (
+        <li
+            key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'
+        >
+            {link}
+        </li>
+    ))}
+
+</ul>
+)}
+
+            
         </div>
     );
 };

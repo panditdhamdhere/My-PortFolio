9776518bb8365cbab93b711e9dc193b198d4e538
pandit <dhamdherepandit@gmail.com>
2022-12-28 17:35:29 +0530

    paragraph added

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
     return (
         <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-blue-500'>
 
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
 
-                <div>
+                <div className='flex flex-col'>
                     <h2>Hi I am Pandit Frontend Web Developer</h2>
                     <p>I am young passionate web developer based in Pune India.
                         I love to solve problems through coding.
@@ -20,8 +20,10 @@ const Home = () => {
                                 <MdKeyboardArrowRight />
                             </span>
                         </button>
+
                     </div>
                 </div>
+                {/* <div><img src={HeroImage} alt="my pic"  className="rounded-2xl mx-auto w-2/3 md:w-full"/></div> */}
             </div>
         </div>
     )

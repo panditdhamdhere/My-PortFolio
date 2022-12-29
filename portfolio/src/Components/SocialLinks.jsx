54540c2d24dd7ed0a8d16e73e33953fe3dd70a65
pandit <dhamdherepandit@gmail.com>
2022-12-29 17:51:31 +0530

    started building social links

---
+++ portfolio/src/Components/SocialLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMails } from 'react-icons/hi'
+import { BsFillPersonalLinesFill } from 'react-icons/bs'
+const SocialLinks = () => {
+    return (
+        <div>
+            SocialLinks
+        </div>
+    )
+}
+
+export default SocialLinks

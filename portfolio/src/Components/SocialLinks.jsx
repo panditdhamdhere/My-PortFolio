3adccd7e7f81e5aca0c8b0ff0669ae1e818ade61
pandit <dhamdherepandit@gmail.com>
2022-12-29 21:11:06 +0530

    linked in icon added

--- portfolio/src/Components/SocialLinks.jsx
+++ portfolio/src/Components/SocialLinks.jsx
@@ -4,8 +4,17 @@ import { HiOutlineMails } from 'react-icons/hi'
 import { BsFillPersonalLinesFill } from 'react-icons/bs'
 const SocialLinks = () => {
     return (
-        <div>
-            SocialLinks
+        <div className=' flex flex-col top-[35%] left-0 fixed'>
+            <ul>
+                <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500'>
+                    <a href="" className='flex justify-between items-center w-full text-white'>
+                       
+                        <>
+                            LinkedIn<FaLinkedin size={30}/>
+                        </>
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }

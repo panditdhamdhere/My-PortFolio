300a8804e63f93c1f6fc24b578c3df8b9b1f6f69
pandit <dhamdherepandit@gmail.com>
2022-12-31 21:55:29 +0530

    about section is completed

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
     return (
         <div name="about" className='w-full h-screen bg-gradient-to-b from-blue-500 to-black text-white'>
 
-            <div>
-                <div>
-                    <p>About</p>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-blue-100'>About</p>
                 </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eligendi soluta harum sunt, non porro assumenda fuga saepe alias pariatur sed, hic inventore aut. Qui nostrum voluptate similique reiciendis deserunt?</p>
+                <p className='text-xl mt-14'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eligendi soluta harum sunt, non porro assumenda fuga saepe alias pariatur sed, hic inventore aut. Qui nostrum voluptate similique reiciendis deserunt?</p>
                 <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit unde earum amet. Quisquam quasi incidunt molestias, voluptatem perferendis architecto illum commodi cumque tempore, rerum, placeat iure repellat enim. Laboriosam, veniam?</p>
+                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit unde earum amet. Quisquam quasi incidunt molestias, voluptatem perferendis architecto illum commodi cumque tempore, rerum, placeat iure repellat enim. Laboriosam, veniam?</p>
 
             </div>
         </div>

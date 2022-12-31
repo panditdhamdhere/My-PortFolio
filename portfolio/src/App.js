c1e6b8142bfc6a1efd8288bf57b2173c1aa400ef
pandit <dhamdherepandit@gmail.com>
2022-12-31 22:24:01 +0530

    added some functionalities

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,14 +4,15 @@ import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import SocialLinks from "./Components/SocialLinks";
 import About from "./Components/About";
+import Portfolio from "./Components/Portfolio";
 function App() {
   return (
     <div>
-     <Navbar />
-     <Home />
-     <About />
-
-     <SocialLinks />
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <SocialLinks />
     </div>
   );
 }

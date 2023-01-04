929d393e064af5eb29fc3941e8b50c7f74adbc24
pandit <dhamdherepandit@gmail.com>
2023-01-04 19:43:52 +0530

    fixing contact

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <div name="contact" className='w-full h-screen bg-gradient-to-b from-black to-blue-500 p-4 text-white'>
             <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-blue-500'>Contact</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-blue-500 pt-4 mt-4'>Contact</p>
                     <p className='py-6'>Submit a form below to get touch with me</p>
                 </div>
 <div className='flex justify-center items-center'>
